dd6585f7b167f25a23764fa08e3b8858194ee28d
SkyWT <me@skywt.cn>
2024-04-07 14:59:02 +0800

    ✨ 删除无用模块等

---
+++ README.md
@@ -0,0 +1,6 @@
+# Starter
+
+配置了 Webpack、Babel、React、Tailwind、TypeScript、ESLint 的前端启动模板。
+
+- master 分支：TypeScript 版本
+- js 分支：JavaScript（无 TS）版本
--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,10 @@
-import React, { Component } from "react";
+import React from "react";
 import "./style.css";
-import moment from "moment";
 
-class App extends Component {
-  render (): React.JSX.Element {
-    return (
-      <div className="App">
-        <h1 className="text-4xl">Hello, World!</h1>
-        <p>{moment().format()}</p>
-      </div>
-    );
-  }
-}
-
-export default App;
+export default function App() {
+  return (
+    <div className="App">
+      <h1 className="text-4xl">Hello, World!</h1>
+    </div>
+  );
+};
