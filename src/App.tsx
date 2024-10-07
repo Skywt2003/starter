7dce6991bc532d1b4c9d0ce419fdc996b9903c7a
SkyWT <me@skywt.cn>
2024-10-07 16:42:25 +0800

    Version upgrade

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./style.css";
 
-export default function App() {
+export default function App (): React.JSX.Element {
   return (
     <div className="App">
       <h1 className="text-4xl">Hello, World!</h1>
